cbe58cec10b41aa6f3779e97150cb9f00899398c
bithighlander <bithighlander@gmail.com>
2023-07-04 14:05:28 -0500

    debug paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -131,10 +130,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/keepkey',
